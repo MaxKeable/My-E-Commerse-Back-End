843770240856c48471b5247bd7fd50f2944e9753
MaxKeable <max.keable488@gmail.com>
2023-05-14 12:07:17 +1000

    adding routes and api end points

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
+// Purpose: API routes
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// Define route prefixes
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
